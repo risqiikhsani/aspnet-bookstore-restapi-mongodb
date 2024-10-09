6bb49598ffa3a382bbe07313139e317eabf50ebc
risqiikhsani <risqiikhsani16@gmail.com>
2024-10-09 22:17:38 +0700

    added note

--- obj/Debug/net8.0/BookStoreApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/BookStoreApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStoreApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e72676329a2f674a289458d0ad7f8057059cc1c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStoreApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStoreApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
